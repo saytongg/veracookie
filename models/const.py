9dc96ed8f469d6a9e4e2869eff14bb391ccf935f
Braullo Jo <bajo@up.edu.ph>
2024-06-22 04:58:17 +0800

    Added docker compose config file

--- models/const.py
+++
@@ -1,9 +0,0 @@
-# Paths to datasets
-LANG_CLARITY_DATASET1_PATH = "../dataset/lang_clarity/ariadne_dataset.csv"
-LANG_CLARITY_DATASET2_PATH = "../dataset/lang_clarity/veracookie_dataset.csv"
-OPT_WEIGHTS_DATASET_PATH = "../dataset/opt_weights"
-
-# Where to load and save learning models
-LANG_CLARITY_MODEL_PATH = "../models/lang_clarity_model.joblib"
-VECTORIZER_PATH = "../models/vectorizer.joblib"
-OPT_WEIGHTS_MODEL_PATH = "../models/opt_weights_model.pth"
